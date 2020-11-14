d789747c4a9761fecdd7e8596c21fae5b9ed2a18
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-11-14 11:36:41 +0100

    V0.0.2 with suggested improvements from https://github.com/obsidianmd/obsidian-releases/pull/41

--- main.ts
+++ main.ts
@@ -1,61 +1,59 @@
-import { Plugin } from "obsidian"
-
-const { clipboard } = require('electron');
+import { Plugin, Notice } from "obsidian"
 
 export default class HighlighExtractor extends Plugin {
-  statusBar: HTMLElement
+	statusBar: HTMLElement
 
-  async onload() {
-    this.statusBar = this.addStatusBarItem()
+	async onload() {
+		this.statusBar = this.addStatusBarItem()
 
-	this.addCommand({
-		id: "shortcut-highlights",
-		name: "Shortcut for extracting highlights",
-		callback: () => this.extractHighlights(),
-		hotkeys: [
-		  {
-			modifiers: ["Alt", "Shift"],
-			key: "±",
-		  },
-		],
-	  });
-  }
+		this.addCommand({
+			id: "shortcut-highlights",
+			name: "Shortcut for extracting highlights",
+			callback: () => this.extractHighlights(),
+			hotkeys: [
+				{
+					modifiers: ["Alt", "Shift"],
+					key: "±",
+				},
+			],
+		});
+	}
 
-  extractHighlights(): void {
-	
-	let activeLeaf: any = this.app.workspace.activeLeaf ?? null
+	extractHighlights(): void {
 
-    try {
-      if (activeLeaf?.view?.data) {
-		let highlights = this.processHighlights(activeLeaf.view.data);
-		let saveStatus = this.saveToClipboard(highlights);
-        this.statusBar.setText(saveStatus);
-      } else {
-        this.statusBar.setText("No highlights to extract.")
-      }
-    } catch (e) {
-      console.log(e.message)
-    }
-  }
+		let activeLeaf: any = this.app.workspace.activeLeaf ?? null
 
-  processHighlights(data: string): string {
-	  let re = /==(.*?)==/g;
-	  let matches = data.match(re);
-	  var result = "## Highlights\n";
+		try {
+			if (activeLeaf?.view?.data) {
+				let highlights = this.processHighlights(activeLeaf.view.data);
+				let saveStatus = this.saveToClipboard(highlights);
+				new Notice(saveStatus);
+			} else {
+				new Notice("No highlights to extract.");
+			}
+		} catch (e) {
+			console.log(e.message)
+		}
+	}
 
-	  for (let entry of matches) {		
-		let removeNewline = entry.replace("\n", "");
-		let removeHighlightStart = removeNewline.replace("==", "")
-		let removeHighlightEnd = removeHighlightStart.replace("==", "")
+	processHighlights(data: string): string {
+		let re = /==(.*?)==/g;
+		let matches = data.match(re);
+		var result = "## Highlights\n";
 
-		result += " - " + removeHighlightEnd + "\n";
-	  }
+		for (let entry of matches) {
+			let removeNewline = entry.replace("\n", "");
+			let removeHighlightStart = removeNewline.replace("==", "")
+			let removeHighlightEnd = removeHighlightStart.replace("==", "")
 
-	  return result;
-  }
+			result += " - " + removeHighlightEnd + "\n";
+		}
 
-  saveToClipboard(data:string): string {
-	clipboard.writeText(data);	
-	return "Highlights copied to clipboard!";
-  }
+		return result;
+	}
+
+	saveToClipboard(data: string): string {
+		navigator.clipboard.writeText(data);
+		return "Highlights copied to clipboard!";
+	}
 }
