1d8ba829fcd5990ee4144a4fd4901425287e1e1a
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-12-03 11:57:10 +0100

    Adds basic line-highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { Plugin, Notice, addIcon, View } from "obsidian"
+import {Plugin, Notice, addIcon, View, MarkdownView} from "obsidian"
 import ExtractHighlightsPluginSettings from "./ExtractHighlightsPluginSettings"
 import ExtractHighlightsPluginSettingsTab from "./ExtractHighlightsPluginSettingsTab"
+import {Position} from "codemirror";
 
 addIcon('target', '<path d="M50 88C29.0132 88 12 70.9868 12 50C12 29.0132 29.0132 12 50 12C70.9868 12 88 29.0132 88 50C87.9761 70.9769 70.9769 87.9761 50 88ZM50 22.8571C35.0094 22.8571 22.8571 35.0094 22.8571 50C22.8571 64.9906 35.0094 77.1429 50 77.1429C64.9906 77.1429 77.1429 64.9906 77.1429 50C77.1429 35.0094 64.9906 22.8571 50 22.8571ZM50 66.2857C41.0056 66.2857 33.7143 58.9943 33.7143 50C33.7143 41.0056 41.0056 33.7143 50 33.7143C58.9943 33.7143 66.2857 41.0056 66.2857 50C66.2857 54.3192 64.5699 58.4616 61.5157 61.5157C58.4616 64.5699 54.3192 66.2857 50 66.2857Z" fill="#646464"/>')
 
@@ -9,6 +10,7 @@ export default class ExtractHighlightsPlugin extends Plugin {
 	public settings: ExtractHighlightsPluginSettings;
 	public statusBar: HTMLElement
 	public counter: 0;
+	private editor: CodeMirror.Editor;
 
 	async onload() {
 		this.counter = 0;
@@ -21,14 +23,36 @@ export default class ExtractHighlightsPlugin extends Plugin {
 			this.extractHighlights();
 		});
 
+		this.registerEvent(
+			this.app.on('codemirror', (cm: CodeMirror.Editor | MarkdownView) => {
+				if ('sourceMode' in cm) {
+					this.editor = cm.sourceMode.cmEditor;
+				} else {
+					this.editor = cm;
+				}
+			}),
+		);
+
 		this.addCommand({
-			id: "shortcut-highlights",
+			id: "shortcut-extract-highlights",
 			name: "Shortcut for extracting highlights",
 			callback: () => this.extractHighlights(),
 			hotkeys: [
 				{
 					modifiers: ["Alt", "Shift"],
 					key: "±",
+				},
+			],
+		});
+
+		this.addCommand({
+			id: "shortcut-highlight-sentence",
+			name: "Shortcut for highlighting sentence cursor is in",
+			callback: () => this.toggleLineHighlight(),
+			hotkeys: [
+				{
+					modifiers: ["Alt", "Shift"],
+					key: "—",
 				},
 			],
 		});
@@ -64,6 +88,28 @@ export default class ExtractHighlightsPlugin extends Plugin {
 		} catch (e) {
 			console.log(e.message)
 		}
+	}
+
+	toggleLineHighlight() {
+		const cursorPosition = this.editor.getCursor();
+		let lineText = this.editor.getLine(cursorPosition.line);
+
+		let highlightedLine = "";
+		let startPosition: Position;
+		let endPosition: Position;
+
+		if(lineText.startsWith("==") && lineText.endsWith("==")) {
+			highlightedLine = lineText.replace(/==/g, "");
+			startPosition = {line: cursorPosition.line, ch: 0};
+			endPosition = {line: cursorPosition.line, ch: highlightedLine.length + 4};
+		} else {
+			highlightedLine = "==" + lineText + "==";
+			startPosition = {line: cursorPosition.line, ch: 0};
+			endPosition = {line: cursorPosition.line, ch: highlightedLine.length};
+		}
+
+		this.editor.replaceRange(highlightedLine, startPosition, endPosition);
+		this.editor.setCursor(cursorPosition);
 	}
 
 	processHighlights(view: any): string {
