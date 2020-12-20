416518a85ad2e25a6e7dca04129fc53d815d2ed6
Alexis Rondeau <alexis.rondeau@gmail.com>
2020-12-20 19:25:42 +0100

    Adds loading of PDF

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {Plugin, Notice, addIcon, View, MarkdownView, Workspace} from "obsidian"
 import ExtractHighlightsPluginSettings from "./ExtractHighlightsPluginSettings"
 import ExtractHighlightsPluginSettingsTab from "./ExtractHighlightsPluginSettingsTab"
-import {Position} from "codemirror";
 import ToggleHighlight from "./ToggleHighlight";
 
 addIcon('target', '<path d="M50 88C29.0132 88 12 70.9868 12 50C12 29.0132 29.0132 12 50 12C70.9868 12 88 29.0132 88 50C87.9761 70.9769 70.9769 87.9761 50 88ZM50 22.8571C35.0094 22.8571 22.8571 35.0094 22.8571 50C22.8571 64.9906 35.0094 77.1429 50 77.1429C64.9906 77.1429 77.1429 64.9906 77.1429 50C77.1429 35.0094 64.9906 22.8571 50 22.8571ZM50 66.2857C41.0056 66.2857 33.7143 58.9943 33.7143 50C33.7143 41.0056 41.0056 33.7143 50 33.7143C58.9943 33.7143 66.2857 41.0056 66.2857 50C66.2857 54.3192 64.5699 58.4616 61.5157 61.5157C58.4616 64.5699 54.3192 66.2857 50 66.2857Z" fill="#646464"/>')
@@ -69,7 +68,24 @@ export default class ExtractHighlightsPlugin extends Plugin {
 	extractHighlights(): void {
 		let activeLeaf: any = this.app.workspace.activeLeaf ?? null
 
+		console.log(this.app.workspace.activeLeaf?.view);
+
 		try {
+			if (this.app.workspace.activeLeaf?.view.icon == "pdf-file") {
+				console.log("Process PDF");
+				var pdfPath = activeLeaf?.view.file.path;
+
+				// pdfjsLib.GlobalWorkerOptions.workerSrc = './node_modules/pdfjs-dist/build/pdf.worker.js'
+
+				console.log(pdfPath);
+
+				const vaultPath = activeLeaf?.view.file.vault.adapter.basePath;
+				const path = "file://" + vaultPath + "/" + pdfPath;
+
+				// @ts-ignore
+				var loadingTask = pdfjsLib.getDocument(path);
+			}
+
 			if (activeLeaf?.view?.data) {
 				let highlightsText = this.processHighlights(activeLeaf.view);
 				let saveStatus = this.saveToClipboard(highlightsText);
